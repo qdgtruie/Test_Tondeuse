87d9ea7ff40585b9e97c2de1345df321f997847c
ysaule <yvan.saule@digitaslbi.com>
2019-06-01 15:20:28 +0200

    adding GKE

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,7 @@ set -e
 
 docker build -t gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
 
+
 echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
