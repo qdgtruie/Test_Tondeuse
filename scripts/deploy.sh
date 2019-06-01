bd1f6e783f324e6987392667edcc9168344c220d
ysaule <yvan.saule@digitaslbi.com>
2019-06-01 15:46:21 +0200

    adding GKE

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,7 @@ gcloud --quiet config set project $PROJECT_NAME_PRD
 gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
+gcloud components install kubectl
 
 gcloud docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}
 
